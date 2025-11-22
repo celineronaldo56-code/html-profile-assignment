7ddeb9582b7b6db466415a78712eebb79aec8864
celineronaldo56-code <celineronaldo56@gmail.com>
2025-11-22 10:39:54 +0100

    Added javascript

---
+++ Javascript Assignment/script.js
@@ -0,0 +1,45 @@
+// Execise 1 (Personal Information)
+const fristName = "Celine";
+const lastName = "Ronaldo";
+let age = 30;
+let isStudent = age===30;
+let subjects = ["English", "Chinese", "Igbo"];
+let favoriteBook = {
+    title: " Things Fall Apart",
+    author: "Chinua Achebe",
+    year: "1958",
+   rating:"10",
+};
+console.log(fristName);
+console.log(lastName);
+console.log(age);
+console.log(subjects);
+console.log(isStudent);
+console.log(favoriteBook.title);
+console.log(favoriteBook.author);
+console.log(favoriteBook.year);
+console.log(favoriteBook.rating);
+
+// Execise 2 (Simple Calculator)
+let sum = 7 + 20;
+let difference = 16 - 7;
+let product = 7 * 7;
+let quotient = 20 /  7;
+let remainder = 14 % 7;
+//let format output: "45 + 12 = 57";
+
+console.log(sum);
+console.log(difference);
+console.log(product);
+console.log(quotient);
+console.log(remainder);
+
+// Execise 3 (Type Conversion)
+let str1 = "150";
+let str2 = "100";
+let num = 77;
+let str = 150===77;
+
+console.log(str1 + str2);
+console.log(num);
+console.log(str);
